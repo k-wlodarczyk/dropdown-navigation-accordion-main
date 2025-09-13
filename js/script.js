cdef89b03396da3e562b12be3e42e488eee39569
k-wlodarczyk <konrad.wlodarczyk99@gmail.com>
2025-09-13 21:36:07 +0200

    Add hiding menu and fix navbar elements positioning

--- js/script.js
+++ js/script.js
@@ -3,19 +3,28 @@
 const hamburgerBtn = document.querySelector(".hamburger-btn");
 const hamburgerIcon = document.querySelector(".hamburger-icon");
 const navMenuEl = document.querySelector(".nav-menu-list");
+const navMenuItemEl = document.querySelector(".nav-menu-item");
 const overlayEl = document.querySelector(".overlay");
+const dropdownMenus = document.querySelectorAll(".dropdown");
 
-const dropdownElements = document.querySelectorAll(
+const dropdownInitiators = document.querySelectorAll(
   ".nav-menu-item[dropdown-element]"
 );
 
-dropdownElements.forEach((item) => {
+dropdownInitiators.forEach((item) => {
   const arrow = item.querySelector(".menu-item-arrow");
   const dropdownMenu = item.querySelector(".dropdown");
 
   item.addEventListener("click", function () {
     arrow.classList.toggle("menu-item-arrow-opened");
     dropdownMenu.classList.toggle("dropdown-opened");
+
+    dropdownInitiators.forEach((otherDropdownInitiator) => {
+      if (otherDropdownInitiator !== item) {
+        const dropdownMenu = otherDropdownInitiator.querySelector(".dropdown");
+        dropdownMenu.classList.remove("dropdown-opened");
+      }
+    });
   });
 });
 
@@ -33,4 +42,13 @@ overlayEl.addEventListener("click", function () {
   navMenuEl.classList.remove("open");
   overlayEl.classList.remove("active");
   hamburgerIcon.src = "images/icon-menu.svg";
+});
+
+document.addEventListener("click", function (e) {
+  dropdownInitiators.forEach((dropdownInitiator) => {
+    const dropdownChild = dropdownInitiator.querySelector(".dropdown");
+    if (!dropdownInitiator.contains(e.target)) {
+      dropdownChild.classList.remove("dropdown-opened");
+    }
+  });
 });
